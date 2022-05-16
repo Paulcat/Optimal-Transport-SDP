15f01534e125e891c3c6fff6889eebf4bd0d4646
Paul Catala <paul.catala@uos.de>
2022-05-16 16:57:17 +0200

    cleaning

--- 1D/Dnumel2.m
+++
@@ -1,9 +0,0 @@
-function C = Dnumel2(m)
-%DNUMEL(N) Number of 'diagonal' elements for 2-level Toeplitz matrix
-
-O = ones(m);
-O = padarray(O,m-1,'post');
-C = ifftshift( ifft2(fft2(O).^2) );
-
-end
-
